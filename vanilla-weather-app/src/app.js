d4715e2d8a6c84ce3413bde6686d021e6a0d4ec0
Viviane G. B <66354969+vihaarman@users.noreply.github.com>
2022-05-08 16:00:26 +0200

    Added CSS

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -28,12 +28,18 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
@@ -49,7 +55,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Maarheeze");
+search("Amsterdam");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
