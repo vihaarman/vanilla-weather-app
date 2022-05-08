16ca74ab7a4f25265a20ddd2f027ee08ee2e2f26
Viviane G. B <66354969+vihaarman@users.noreply.github.com>
2022-05-08 11:07:50 +0200

    Added Search Engine

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+function search(city) {
+  let apiKey = "17235f17a284399dab836bca23a36655";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement.value);
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
