f10bb52ec8bab5322ba030bb3586a1c605add81a
Viviane G. B <66354969+vihaarman@users.noreply.github.com>
2022-05-15 13:51:11 +0200

    Added Forecast / API Integration

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,28 +21,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}: ${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-  let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `           
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `           
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="36"
           />
           <div class="weather-forecast-temperatures">
-            <span class="temperature-max"> 23°C </span>
-            <span class="temperature-min"> 20°C </span>
+            <span class="temperature-max"> ${Math.round(
+              forecastDay.temp.max
+            )}°C </span>
+            <span class="temperature-min"> ${Math.round(
+              forecastDay.temp.min
+            )}°C </span>
           </div>
         </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
